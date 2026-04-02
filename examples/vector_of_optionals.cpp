691ea9cca442738a527b14eb517d11e146170330
Braden Ganetsky <braden.ganetsky@gmail.com>
2026-04-02 16:01:13 +0100

    Add missing copyright notices

--- examples/vector_of_optionals.cpp
+++ examples/vector_of_optionals.cpp
@@ -1,3 +1,7 @@
+// Copyright 2025 Braden Ganetsky
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+
 #include <k3/k3tchup_main.hpp>
 #include <optional>
 #include <vector>
