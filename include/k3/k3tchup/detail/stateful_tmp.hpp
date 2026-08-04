57e17b5f127458022372ef259852b3cd12d1b332
Braden Ganetsky <braden.ganetsky@gmail.com>
2026-08-03 23:35:16 -0500

    Ignore GCC warning non-template-friend

--- include/k3/k3tchup/detail/stateful_tmp.hpp
+++ include/k3/k3tchup/detail/stateful_tmp.hpp
@@ -9,7 +9,18 @@ namespace k3::k3tchup::detail {
 
 template <class T>
 struct error_key {
+
+#if defined(__GNUC__) && !defined(__clang__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wnon-template-friend"
+#endif
+
     friend auto find(error_key<T>);
+
+#if defined(__GNUC__) && !defined(__clang__)
+#pragma GCC diagnostic pop
+#endif
+
 };
 
 template <class T>
